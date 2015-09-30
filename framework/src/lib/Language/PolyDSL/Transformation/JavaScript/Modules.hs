ca34b4add400bf72ed8aaf989be057f88a442d19
Nils 'bash0r' Jonsson <aka.bash0r@gmail.com>
2015-09-30 14:47:02 +0200

    Cleaned implementation of module transformations.

--- framework/src/lib/Language/PolyDSL/Transformation/JavaScript/Modules.hs
+++ framework/src/lib/Language/PolyDSL/Transformation/JavaScript/Modules.hs
@@ -51,26 +51,30 @@ import qualified Language.PolyDSL.DOM as DOM
 import           Language.PolyDSL.Transformation.JavaScript.GADTs
 import           Language.PolyDSL.Transformation.JavaScript.Internal
 
+
+-- | A register for modules.
 moduleRegister = this ... "$module_register$"
 
+
+filterDecls []                        is ts tas fs = (reverse is, reverse ts, reverse tas, reverse fs)
+filterDecls (i@(DOM.Import    {}):ds) is ts tas fs = filterDecls ds (i:is) ts     tas     fs
+filterDecls (t@(DOM.GADT      {}):ds) is ts tas fs = filterDecls ds is     (t:ts) tas     fs
+filterDecls (t@(DOM.TypeAlias {}):ds) is ts tas fs = filterDecls ds is     ts     (t:tas) fs
+filterDecls (f@(DOM.Function  {}):ds) is ts tas fs = filterDecls ds is     ts     tas     (f:fs)
+filterDecls (s@(DOM.Signature {}):ds) is ts tas fs = filterDecls ds is     ts     tas     (s:fs)
+
+
 instance Transformer DOM.Module (SemanticResult ModL1) where
-  transform (DOM.Module mName es ds) =
+  transform (DOM.Module mName es ds) = do
       let (is, ts, tas, fs) = filterDecls ds [] [] [] []
-       in return $ ModL1 mName es (Imports is) (GADTs ts) (TypeAliases tas) (Functions fs)
-    where
-      filterDecls []                        is ts tas fs = (reverse is, reverse ts, reverse tas, reverse fs)
-      filterDecls (i@(DOM.Import    {}):ds) is ts tas fs = filterDecls ds (i:is) ts     tas     fs
-      filterDecls (t@(DOM.GADT      {}):ds) is ts tas fs = filterDecls ds is     (t:ts) tas     fs
-      filterDecls (t@(DOM.TypeAlias {}):ds) is ts tas fs = filterDecls ds is     ts     (t:tas) fs
-      filterDecls (f@(DOM.Function  {}):ds) is ts tas fs = filterDecls ds is     ts     tas     (f:fs)
-      filterDecls (s@(DOM.Signature {}):ds) is ts tas fs = filterDecls ds is     ts     tas     (s:fs)
+      return (ModL1 mName es (Imports is) (GADTs ts) (TypeAliases tas) (Functions fs))
 
 instance Transformer ModL1 (SemanticResult Statement) where
-  transform (ModL1 mName es is ts tas fs) = return $ expr (moduleRegister ... mName .= body)
-    where
-      body = new (function [] (defs ++ exps)) []
-      defs = transform ts
-      exps = map (\v -> expr (this ... v .= ident v)) es
+  transform (ModL1 mName es is ts tas fs) = do
+    ts' <- transform ts
+    let body = new (function [] (ts' ++ exps)) []
+        exps = map (\v -> expr (this ... v .= ident v)) es
+    return $ expr (moduleRegister ... mName .= body)
 
 instance Transformer DOM.Module (SemanticResult Statement) where
   transform p = do
